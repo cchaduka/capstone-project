5b579cc4a744352541fafd541f0629a705501cce
Christopher Chaduka <kiri@kiri.co.zw>
2020-09-16 23:14:08 +0200

    Update kubernetes script

--- scripts/k8s-deploy.sh
+++ scripts/k8s-deploy.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+eksctl create cluster --name=capstonecluster --version=1.17 --nodegroup-name=capstonenodegroup --node-type=t2.medium --nodes=3 --nodes-min=1 --nodes-max=4 --node-ami=auto --region=us-west-2
